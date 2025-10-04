8e12310af5a8e57cf1296b93f5a43123328e4a3b
jefferson.sotto <jeffersontads@hotmail.com>
2025-10-04 15:56:25 -0300

    feat: enhance character list and favorites functionality with search and pagination

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class Character {
-  
+export class CharacterService {
+  private apiUrl = 'https://rickandmortyapi.com/api';
+
+  constructor(private http: HttpClient) {}
+
+  getCharacters(page: number = 1, name: string = ''): Observable<any> {
+    let url = `${this.apiUrl}/character/?page=${page}`;
+    if (name) {
+      url += `&name=${name}`;
+    }
+    return this.http.get(url);
+  }
+
+  getCharacter(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/character/${id}`);
+  }
+
+  getEpisodes(episodeUrls: string[]): Observable<any> {
+    const episodeIds = episodeUrls.map((url) => url.split('/').pop());
+    return this.http.get(`${this.apiUrl}/episode/${episodeIds}`);
+  }
 }
--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -1,8 +1,58 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class Favorites {
-  
+export class FavoriteService {
+  private favorites: any[] = [];
+  private favoritesSubject = new BehaviorSubject<any[]>([]);
+
+  constructor() {
+    this.loadFavorites();
+  }
+
+  private loadFavorites(): void {
+    const storedFavorites = localStorage.getItem('favorites');
+    if (storedFavorites) {
+      this.favorites = JSON.parse(storedFavorites);
+      this.favoritesSubject.next(this.favorites);
+    }
+  }
+
+  private saveFavorites(): void {
+    localStorage.setItem('favorites', JSON.stringify(this.favorites));
+    this.favoritesSubject.next(this.favorites);
+  }
+
+  getFavorites() {
+    return this.favoritesSubject.asObservable();
+  }
+
+  addFavorite(character: any): void {
+    if (!this.isFavorite(character.id)) {
+      this.favorites.push(character);
+      this.saveFavorites();
+    }
+  }
+
+  removeFavorite(id: number): void {
+    const index = this.favorites.findIndex((char) => char.id === id);
+    if (index !== -1) {
+      this.favorites.splice(index, 1);
+      this.saveFavorites();
+    }
+  }
+
+  toggleFavorite(character: any): void {
+    if (this.isFavorite(character.id)) {
+      this.removeFavorite(character.id);
+    } else {
+      this.addFavorite(character);
+    }
+  }
+
+  isFavorite(id: number): boolean {
+    return this.favorites.some((char) => char.id === id);
+  }
 }
