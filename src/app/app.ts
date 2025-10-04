e944f90e9699717831ff27e20a4a564d2802e208
jefferson.sotto <jeffersontads@hotmail.com>
2025-10-04 19:17:15 -0300

    feat: refactor app and favorites components for improved structure and functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HttpClientModule, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('rickmorty-challenge');
+  title = 'Rick and Morty Challenge';
 }
--- src/app/components/favorites/favorites.ts
+++ src/app/components/favorites/favorites.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FavoriteService } from '../../services/favorites.service';
 
 @Component({
   selector: 'app-favorites',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './favorites.html',
-  styleUrl: './favorites.scss'
+  styleUrl: './favorites.scss',
 })
-export class Favorites {
+export class Favorites implements OnInit {
+  favorites: any[] = [];
 
+  constructor(private favoriteService: FavoriteService) {}
+
+  ngOnInit(): void {
+    this.favoriteService.getFavorites().subscribe((favorites) => {
+      this.favorites = favorites;
+    });
+  }
+
+  removeFromFavorites(character: any): void {
+    this.favoriteService.removeFavorite(character.id);
+  }
 }
