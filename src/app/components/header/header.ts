349aa773e61654d4717fbfebc58a6f82279d3844
jefferson.sotto <jeffersontads@hotmail.com>
2025-10-05 03:00:53 -0300

    feat(header): update logo and styling
    - Replace logo with new Rick and Morty logo
    - Simplify header layout and styling
    - Update active/hover colors for navigation
    - Remove unused imports and add logo path variable

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 import { FavoriteService } from '../../services/favorites.service';
 import { LanguageSelector } from '../language-selector/language-selector';
-import { TranslatePipe } from '../../pipes/translate.pipe';
+
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive, LanguageSelector, TranslatePipe],
+  imports: [RouterLink, LanguageSelector],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header implements OnInit, OnDestroy {
   favoriteCount: number = 0;
   private destroy$ = new Subject<void>();
+  logoPath = 'assets/logo/rick-morty-logo.png';
 
   constructor(private favoriteService: FavoriteService) {}
 
